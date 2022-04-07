bd7afd07e17fe326243a3528d0f24fced0526426
forysiak-vova <volodja.forus@gmail.com>
2022-04-08 00:42:29 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="goit-react-hw-08-phonebook">
+        <BrowserRouter basename="/oit-react-hw-08-phonebook/">
           <ThemeProvider theme={theme}>
         <App />
           </ThemeProvider>
